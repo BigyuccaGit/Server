8cb9102fccf9385c2472accbcd6ac08e02938af2
Howard Huckle <howardhuckle@yahoo.co.uk>
2021-10-29 15:54:14 +0100

    Added comments, tweaked some filename

--- helloWorld.py
+++ helloWorld.py
@@ -1,7 +1,13 @@
 from flask import Flask
+
+# Create a Flask object
 app = Flask(__name__)
+
+# Run index function when someone accesses the root URL ('/') of teh server
 @app.route('/')
 def index():
     return 'Hello world'
+
+# Listen on port 80
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
--- helloWorldTemplate.py
+++ helloWorldTemplate.py
@@ -4,15 +4,23 @@ for details, visit:  http://mattrichardson.com/Raspberry-Pi-Flask/inde...
 '''
 from flask import Flask, render_template
 import datetime
+
+# Create a Flask object
 app = Flask(__name__)
-@app.route("/hello")
+
+# Run index function when someone accesses the root URL ('/') of the server
+@app.route("/")
 def hello():
+   # Info for display
    now = datetime.datetime.now()
    timeString = now.strftime("%Y-%m-%d %H:%M")
    templateData = {
       'title' : 'HELLO!',
       'time': timeString
       }
+   # Render template in templates dir
    return render_template('index.html', **templateData)
+
+# Listen on port 80
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True)
